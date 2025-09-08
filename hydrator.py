3482a1be3ed5ff784fe5f37550d990133995106f
Asmaar Abid <asmar_abid@hotmail.com>
2025-09-08 17:16:45 +0200

    added py file

---
+++ hydrator.py
@@ -0,0 +1,45 @@
+import rumps
+import os
+
+class HydrationReminderApp(rumps.App):
+    def __init__(self):
+        super().__init__("💧 Hydration")
+
+        self.timer = rumps.Timer(self.show_alert, 30 * 60)
+        self.timer.start()
+        self.first_run = True
+        self.is_running = False
+
+        # Menu options
+        self.menu = ["30 min", "1 hour"]
+
+    @rumps.clicked("30 min")
+    def set_30min(self, _):
+        self.timer.stop()
+        self.timer.interval = 30 * 60
+        self.timer.start()
+        self.show_confirmation("Timer set for 30 minutes")
+        
+
+    @rumps.clicked("1 hour")
+    def set_1hour(self, _):
+        self.timer.stop()
+        self.timer.interval = 60 * 60
+        self.timer.start()
+        self.show_alert("Hydration!", "Timer set for 1 hour")
+        
+    def show_confirmation(self, message):
+        command = f'''osascript -e 'display dialog "{message}" with title "Hydration" buttons {{"OK"}} default button "OK"' '''
+        os.system(command)
+
+
+
+    def show_alert(self, title="Hydration!", message="Time to hydrate"):
+        if self.first_run:
+            self.first_run = False
+            return
+        command = f'''osascript -e 'display dialog "{message}" with title "{title}" buttons {{"OK"}} default button "OK"' '''
+        os.system(command)
+
+if __name__ == "__main__":
+    HydrationReminderApp().run()
